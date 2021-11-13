e48ca4094469d95b1368291c29b12aac3b0d9bc2
Samuel Riveros <sam_riveros18@outlook.com>
2021-11-13 18:26:22 -0500

    I added the random generators for each password parameter, I also finished promting the user for each said parameter.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//holds boolean inputs from user
+var confirmSymbolChar;
+var confirmNumericChar;
+var confirmLowerChar;
+var confirmUpperChar;
+
+var randomSymbol = '!"#$%&\'()*+,-./:;<=>@[\\]^-`{|}~';
+
 // Write password to the #password input
 function writePassword() {
-  // first prompt for the user. Must be between 8-128 charecters long
-  var charLengthPrompt = window.prompt("How many charecters would you like your password to contain?");
-
-// closes program if user hits cancel on the first promt
-  if (!charLengthPrompt){
-      return;
-  }
-
-  else if (charLengthPrompt < 8){
-      window.alert("Password length must be at least 8 characters.")
-      return;
-  }
-
-  else if(charLengthPrompt > 128){
-    window.alert("Password length must be less than 128 characters.")
-    return;
-  }
-
-  else if(isNaN(charLengthPrompt)){
-    window.alert("Password length must be provided as a number.")
-    return;
-  }
-
-  else{
-      window.confirm("Click OK to confirm including special charracters")
-      return true;
-  }
-
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-
-
 
   passwordText.value = password;
 
@@ -43,3 +20,57 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// random generators******************************************************************
+function getRandomLower(){
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper(){
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);   
+}
+
+function getRandomNumeric(){
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol(){
+    return randomSymbol [Math.floor(Math.random()*randomSymbol.length)];
+}
+
+//*************************************************************************************
+
+//gets input form the user 
+function generatePassword(){
+
+    //first prompt for the user must be between 8-128 characters long
+    var charLengthPrompt = window.prompt("How many charecters would you like your password to contain?");
+  
+    // closes program if user hits cancel on the first promt
+    if (!charLengthPrompt){
+        return;
+    }
+  
+    else if (charLengthPrompt < 8){
+        window.alert("Password length must be at least 8 characters.")
+        return;
+      }
+  
+    else if(charLengthPrompt > 128){
+      window.alert("Password length must be less than 128 characters.")
+      return;
+    }
+  
+    else if(isNaN(charLengthPrompt)){
+      window.alert("Password length must be provided as a number.")
+      return;
+    }
+  
+    else{
+       confirmSymbolChar = window.confirm("Click OK to confirm including special charracters");
+       confirmNumericChar = window.confirm("Click OK to confirm including numeric charracters");
+       confirmLowerChar = window.confirm("Click OK to confirm including Lowercase charracters");
+       confirmUpperChar = window.confirm("Click OK to confirm including Upppercase charracters");
+       
+      }
+};
