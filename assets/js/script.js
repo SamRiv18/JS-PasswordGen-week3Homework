b50a3d8a98bc9fdc395c57cc865ae69b193a90a9
Samuel Riveros <sam_riveros18@outlook.com>
2021-11-13 16:04:51 -0500

    I have added code to prompt the user to choose the lenght for their password between 8 and 128. the program response accordingly to the user input.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,39 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  // first prompt for the user. Must be between 8-128 charecters long
+  var charLengthPrompt = window.prompt("How many charecters would you like your password to contain?");
+
+// closes program if user hits cancel on the first promt
+  if (!charLengthPrompt){
+      return;
+  }
+
+  else if (charLengthPrompt < 8){
+      window.alert("Password length must be at least 8 characters.")
+      return;
+  }
+
+  else if(charLengthPrompt > 128){
+    window.alert("Password length must be less than 128 characters.")
+    return;
+  }
+
+  else if(isNaN(charLengthPrompt)){
+    window.alert("Password length must be provided as a number.")
+    return;
+  }
+
+  else{
+      window.confirm("Click OK to confirm including special charracters")
+      return true;
+  }
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
+
+
 
   passwordText.value = password;
 
